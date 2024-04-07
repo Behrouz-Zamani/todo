8704ccf41e1daf894c022ee74cfb0b0c266efbb0
zamani.behrouz <zamani.behrouz@hotmail.com>
2024-04-07 22:21:10 +0330

    Object distrachering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import TopForm from './TopForm';
 import TaskList from './TaskList';
+import { taskContext } from './taskContext';
 
 const App = () => {
+    const [taskItem, setTaskItem] = useState([
+        {
+            id: 1,
+            title: "Work # 1",
+            done:true
+        },
+        {
+            id: 2,
+            title: "Work # 2",
+            done:false
+        },
+        {
+            id: 3,
+            title: "Work # 3",
+            done:true
+        },
+        {
+            id: 4,
+            title: "Work # 4",
+            done:true
+        },
+    ])
+
     return (
-        <>
-            <TaskList />
-        </>
+        <div className='container w-100 h-100 p-3'>
+            <div className='row h-100 justify-content-center align-align-items-start'>
+                <div className='col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit'>
+            <taskContext.Provider value={{
+                taskItem,
+                setTaskItem
+            }}>
+               
+                <TaskList />
+            </taskContext.Provider>
+            </div>
+            </div>
+        </div>
     )
 }
 
