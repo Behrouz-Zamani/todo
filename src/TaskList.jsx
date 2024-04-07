8afa244c8738d83ef6e92a06dae08e4e431fff7b
zamani.behrouz <zamani.behrouz@hotmail.com>
2024-04-08 00:26:01 +0330

    .

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -3,19 +3,21 @@ import { taskContext } from "./taskContext";
 
 const TaskList = () => {
     const { taskItem, setTaskItem } = useContext(taskContext);
+    //#region  handler
 
-        const handleSetDoneTask =(id) => {
-            const index=taskItem.findIndex(t=> t.id === id);
-            let newTaskItems = [...taskItem]
-            newTaskItems[index].done = !newTaskItems[index].done;
-            setTaskItem(newTaskItems);
-        }
+    const handleSetDoneTask = (id) => {
+        const index = taskItem.findIndex(t => t.id === id);
+        let newTaskItems = [...taskItem]
+        newTaskItems[index].done = !newTaskItems[index].done;
+        setTaskItem(newTaskItems);
+    }
 
-        const handleDeletetask =(id)=> {
-            let newTasks = taskItem.filter(t=>t.id !== id);
-            setTaskItem (newTasks);
-        }
-    if(taskItem.length){
+    const handleDeletetask = (id) => {
+        let newTasks = taskItem.filter(t => t.id !== id);
+        setTaskItem(newTasks);
+    }
+    //#endregion
+    if (taskItem.length) {
         return (
 
             <ul className='list-group M-0 P-0 MT-2'>
@@ -26,11 +28,11 @@ const TaskList = () => {
                             <span>
                                 {
                                     t.done ? (
-                                    <i className='me-3 poimer fas fa-times text-warning transition_200 text_hover_shadow' onClick={()=>{return handleSetDoneTask(t.id)}}></i>) :
-                                     (<i className='me-3 poimer fas fa-check text-success transition_200 text_hover_shadow' onClick={()=>{return handleSetDoneTask(t.id)}}></i>
-                                     )
+                                        <i className='me-3 poimer fas fa-times text-warning transition_200 text_hover_shadow' onClick={() => { return handleSetDoneTask(t.id) }}></i>) :
+                                        (<i className='me-3 poimer fas fa-check text-success transition_200 text_hover_shadow' onClick={() => { return handleSetDoneTask(t.id) }}></i>
+                                        )
                                 }
-                                <i className='me-3 poimer fas fa-trash text-danger transition_200 text_hover_shadow' onClick={()=> {handleDeletetask(t.id)}}></i>
+                                <i className='me-3 poimer fas fa-trash text-danger transition_200 text_hover_shadow' onClick={() => { handleDeletetask(t.id) }}></i>
                             </span>
                         </li>
                     ))
@@ -39,13 +41,13 @@ const TaskList = () => {
             </ul>
 
         )
-    }else{
-        return(
+    } else {
+        return (
             <h4 className="text-center text-danger">No Work Saved!</h4>
         )
 
     }
-   
+
 }
 
 export default TaskList
